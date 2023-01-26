c69da0dd169c7d03b6e2e7e81c9c7c40b5eaa0c4
V-Coder <v.s.tech0990@gmail.com>
2023-01-26 09:43:22 +0530

    Initialized both Frontend and Backend

---
+++ HBackend/src/main/java/com/example/demo/HBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HBackendApplication.class, args);
+	}
+
+}
